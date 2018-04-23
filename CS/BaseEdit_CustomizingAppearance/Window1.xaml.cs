a8ed411b252efdfffb8846dc90e885d71611b252
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/BaseEdit_CustomizingAppearance/Window1.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace BaseEdit_CustomizingAppearance {
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to customize the appearance of a standalone editor
+
+
+<p>The following example shows how to customize the visual appearance of a standalone editor.</p>
+
+<br/>
+
+
